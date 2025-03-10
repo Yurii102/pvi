e0e1c0a89ee3cad609ab791bef147bf8efca144d
Yurii Surniak <surniakyura@gmail.com>
2025-03-11 01:21:18 +0200

    Completion of the header for the first lab

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ const bell = document.getElementById('bell');
 const notificationDot = document.getElementById('notificationDot');
 const notificationPopup = document.getElementById('notificationPopup');
 const bellIcon = document.getElementById('bellIcon');
+const profile = document.getElementById('user');
+const profilePopup = document.getElementById('profilePopup');
 
+let clickTimer;
 bell.addEventListener('click', () => {
-    window.open('messages.html', '_blank');
-    notificationDot.style.display = 'none';
+    clearTimeout(clickTimer); 
+    clickTimer = setTimeout(() => {
+        window.open('messages.html', '_blank');
+        notificationDot.style.display = 'none';
+    }, 300);
+});
 
+bellIcon.addEventListener('dblclick', () => {
+    clearTimeout(clickTimer);
+    bellIcon.classList.add('bell-animation');
+    
+    bellIcon.addEventListener('animationend', () => {
+        notificationDot.style.display = 'block';
+        
+        bellIcon.classList.remove('bell-animation');
+    }, { once: true });
 });
 
 bell.addEventListener('mouseover', () => {
@@ -15,4 +31,12 @@ bell.addEventListener('mouseover', () => {
 
 bell.addEventListener('mouseout', () => {
     notificationPopup.style.display = 'none';
+});
+
+profile.addEventListener('mouseover', () => {
+    profilePopup.style.display = 'block';
+});
+
+profile.addEventListener('mouseout', () => {
+    profilePopup.style.display = 'none';
 });
