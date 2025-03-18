457f9c0c2f8a2a9238ae2c7d6b7f67dbf508340c
Yurii Surniak <surniakyura@gmail.com>
2025-03-19 00:10:09 +0200

    Title update

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,9 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".sidebar a"); 
-    const currentPath = window.location.pathname.split("/").pop(); 
-    links.forEach(link => {
-        if (link.getAttribute("href") === `./${currentPath}`) {
-            link.classList.add("active"); 
-        }
-    });
+const links = document.querySelectorAll(".sidebar a"); 
+const currentPath = window.location.pathname.split("/").pop(); 
+links.forEach(link => {
+    if (link.getAttribute("href") === `./${currentPath}`) {
+        link.classList.add("active"); 
+    }
 });
